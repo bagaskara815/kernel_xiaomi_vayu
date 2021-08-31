e003c335fd97cb1e4fa72b3a5a0e1184a909d59f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-31 07:52:00 +0530

    dsm/msm: dsi-staging: Add support for CABC mode
    * Content Adaptive Backlight Control
    * borrowed code from HBM
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -5465,6 +5465,9 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	if (panel->hbm_mode)
 		dsi_panel_apply_hbm_mode(panel);
 
+	if (panel->cabc_mode)
+		dsi_panel_apply_cabc_mode(panel);
+
 	return rc;
 }
 
@@ -5865,6 +5868,31 @@ int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
 	if (panel->hbm_mode >= 0 &&
 		panel->hbm_mode < ARRAY_SIZE(type_map))
 		type = type_map[panel->hbm_mode];
+	else
+		type = type_map[0];
+
+	mutex_lock(&panel->panel_lock);
+	rc = dsi_panel_tx_cmd_set(panel, type);
+	mutex_unlock(&panel->panel_lock);
+
+	return rc;
+}
+
+int dsi_panel_apply_cabc_mode(struct dsi_panel *panel)
+{
+	static const enum dsi_cmd_set_type type_map[] = {
+		DSI_CMD_SET_DISP_CABCOFF,
+		DSI_CMD_SET_DISP_CABCUION,
+		DSI_CMD_SET_DISP_CABCSTILLON,
+		DSI_CMD_SET_DISP_CABCMOVIEON
+	};
+
+	enum dsi_cmd_set_type type;
+	int rc;
+
+	if (panel->cabc_mode >= 0 &&
+		panel->cabc_mode < ARRAY_SIZE(type_map))
+		type = type_map[panel->cabc_mode];
 	else
 		type = type_map[0];
 
