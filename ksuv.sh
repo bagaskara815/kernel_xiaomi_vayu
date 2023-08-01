71ca4a7a42ab7b3a5db6b42ad45b18a1f3c593a3
orges <orges@orgesified.com>
2023-08-01 20:13:40 +0000

    update ksu version setter
    Signed-off-by: orges <orges@orgesified.com>

--- ksuv.sh
+++ ksuv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-sed -i "s/ifeq (.*)/ifeq (,,)/g;s/ifndef EXPECTED_SIZE/KSU_GIT_VERSION ?= 69\nexport KSU_GIT_VERSION\n\$(eval KSU_VERSION=\$(shell expr 10000 + $\(KSU_GIT_VERSION) + 200))\nccflags-y += -DKSU_VERSION=\$(KSU_VERSION)\n\nifndef EXPECTED_SIZE/g" drivers/kernelsu/Makefile
+sed -i "s/ifeq (.*)/ifeq (,,)/g;s/ifndef KSU_EXPECTED_SIZE/KSU_GIT_VERSION ?= 69\nexport KSU_GIT_VERSION\n\$(eval KSU_VERSION=\$(shell expr 10000 + $\(KSU_GIT_VERSION) + 200))\nccflags-y += -DKSU_VERSION=\$(KSU_VERSION)\n\nifndef KSU_EXPECTED_SIZE/g" drivers/kernelsu/Makefile
 export KSU_GIT_VERSION=$(curl -s https://github.com/tiann/KernelSU | grep d-sm-inline -A1 | grep -oE ">[,0-9]+<" | grep -oE "[,0-9]+" | tr -d ',')
